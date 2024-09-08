c6fa361820a8d4508a0f541605312c5c8d44f36f
Pratik <barvepratik96@gmail.com>
2024-09-09 00:23:12 +0100

    Update run_job.sh

--- run_job.sh
+++ run_job.sh
@@ -2,8 +2,7 @@
 
 # Activate the conda environment
 source /Users/pratik/opt/miniconda3/etc/profile.d/conda.sh
+cd /Users/pratik/Github/Job-finder
 conda activate SETU
-
 python /Users/pratik/Github/Job-finder/jobscraper.py
-
 conda deactivate
